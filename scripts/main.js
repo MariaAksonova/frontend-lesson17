0f018294475ace8e39d658309853cc42903f2ddd
Maria Aksonova <mashaaks@ukr.net>
2017-02-22 22:46:09 +0200

    SCSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
-/**
- * Created by Masha on 17.02.2017.
- */
+$(function (){
+    $('input').on('blur', function () {
+        if (this.value.length < 3) {
+            $(this).addClass('red-frame');
+        } else {
+            $(this).removeClass('red-frame');
+        }
+    });
+
+    $('form').on('submit', function(event) {
+        var canSend = true;
+        $(this).find('input').each(function () {
+            if (this.value.length < 3) {
+                $(this).addClass('red-frame');
+                canSend = false;
+            }
+        });
+        if (!canSend) event.preventDefault();
+    });
+
+    var flag = true;
+    $('.mobile-menu').on('click', function () {
+        if(flag) {
+            flag = false;
+
+            $('.main-header .nav').slideToggle(function () {
+                flag = true;
+            });
+        }
+    })
+});
